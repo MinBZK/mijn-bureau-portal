58c067491aa6fbe3e339813926acad6b014f0a5b
Berry den Hartog <38954346+berrydenhartog@users.noreply.github.com>
2025-02-01 21:03:00 +0100

    Add info box

---
+++ src/assets/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector(".rvo-alert .rvo-button__close")
+    .addEventListener("click", function (event) {
+      const parrent = event.target.closest(".rvo-alert");
+      parrent.style.display = "none";
+    });
+});
